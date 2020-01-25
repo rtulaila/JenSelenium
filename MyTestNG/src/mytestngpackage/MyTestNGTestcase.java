34dc5a730b495a471ca394413bf42445fd18b891
Ramesh <rameshkqa@gmail.com>
2020-01-25 17:49:05 -0500

    My TestNG first commit

---
+++ MyTestNG/src/mytestngpackage/MyTestNGTestcase.java
@@ -0,0 +1,35 @@
+package mytestngpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
+
+public class MyTestNGTestcase {
+	
+	public String baseUrl = "https://www.google.ca/";
+	String driverPath = "C:\\Users\\Ramesh\\Documents\\Automation\\Drivers\\chromedriver.exe";
+	public WebDriver driver;
+	
+	@BeforeTest
+	 public void launchBrowser() {
+		System.out.println("Launching Chrome Bowser");
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+	}
+	
+  @Test
+  public void verifyHomepageTitle() {
+	  String expectedTitle = "Google";
+	  String actualTitle = driver.getTitle();
+	  Assert.assertEquals(actualTitle, expectedTitle);
+	}
+  
+  @AfterTest
+   public void closeBroswer() {
+	  driver.close();
+  }
+   
+}
